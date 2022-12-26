c8bf809b4941b037d6fc9e912b3f1515b4480d3f
bbmenevse <bbmenevse@gmail.com>
2022-12-26 15:13:08 +0300

    Added model to Index and IndexController. Fixed data.sql as i copied bootstrap data and mine was different from the data used in bootstrap. Added Lob to directions cause it raised error for being too long.

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,9 +1,27 @@
 package guru.springframework.controllers;
-
+import guru.springframework.services.RecipeService;
+import guru.springframework.services.RecipeServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 public class IndexController {
 
+    private final RecipeService recipeService;
 
+    @Autowired
+    public IndexController(RecipeServiceImpl recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @RequestMapping({"","/","index","/index"})
+    public String getIndexPage(Model model)
+    {
+        model.addAttribute("recipes",recipeService.getRecipes());
+
+        return "index";
+    }
 }
--- src/main/java/guru/springframework/services/RecipeServiceImpl.java
+++ src/main/java/guru/springframework/services/RecipeServiceImpl.java
@@ -2,10 +2,11 @@ package guru.springframework.services;
 
 import guru.springframework.domain.Recipe;
 import guru.springframework.repositories.RecipeRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
-
+@Service
 public class RecipeServiceImpl implements RecipeService{
 
     private final RecipeRepository recipeRepository;
