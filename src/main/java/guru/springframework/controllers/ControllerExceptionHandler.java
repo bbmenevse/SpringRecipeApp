16df40b4cf04de97b5f2fc4a56a2fceddfaf12b5
bbmenevse <bbmenevse@gmail.com>
2024-03-01 22:17:13 +0300

    added NumberFormatException back as it prevents wrong entries (For example, expected Long but Got String)

--- src/main/java/guru/springframework/controllers/ControllerExceptionHandler.java
+++ src/main/java/guru/springframework/controllers/ControllerExceptionHandler.java
@@ -1,8 +1,10 @@
 package guru.springframework.controllers;
 
 import guru.springframework.exceptions.NotFoundException;
+import jakarta.servlet.http.HttpServletResponse;
 import org.apache.coyote.BadRequestException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -22,32 +24,35 @@ public class ControllerExceptionHandler {
     }
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NotFoundException.class)
-    public ModelAndView handleNotFound(Exception exception) {
-        return genericExceptionMethod(exception, "404 Not Found");
+    public ModelAndView handleNotFound(Exception exception,HttpServletResponse response) {
+        // Wanted to see If I can check status and stuff with response
+        // Commented out since I don't need them at this time
+        //response.setStatus(HttpStatus.NOT_FOUND.value());
+        return genericExceptionMethod(exception, "404 Not Found",response);
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(BadRequestException.class)
-    public ModelAndView handleBadRequest(Exception exception) {
-        return genericExceptionMethod(exception, "400 Bad Request");
+    @ExceptionHandler({NumberFormatException.class, BadRequestException.class})
+    public ModelAndView handleBadRequest(Exception exception, HttpServletResponse response) {
+        //response.setStatus(HttpStatus.BAD_REQUEST.value());
+        return genericExceptionMethod(exception, "400 Bad Request",response);
     }
 
     // If the Spring class for HttpRequestMethodNotSupportedException is called, It will catch it
     // If I define it myself, I will need to throw it where it needs to be thrown
     @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    public ModelAndView handleMethodNotAllowed(Exception exception) {
-        return genericExceptionMethod(exception, "405 Method Not Allowed");
+    public ModelAndView handleMethodNotAllowed(Exception exception,HttpServletResponse response) {
+        //response.setStatus(HttpStatus.METHOD_NOT_ALLOWED.value());
+        return genericExceptionMethod(exception, "405 Method Not Allowed",response);
     }
 
-    private ModelAndView genericExceptionMethod(Exception exception, String response) {
-
+    private ModelAndView genericExceptionMethod(Exception exception, String message,HttpServletResponse response) {
         ModelAndView modelAndView = new ModelAndView();
-
         modelAndView.setViewName("exceptions/400error");
-        modelAndView.addObject("response",response);
+        modelAndView.addObject("message",message);
         modelAndView.addObject("exception", exception);
-
+        //modelAndView.setStatus(HttpStatusCode.valueOf(response.getStatus()));
         return modelAndView;
     }
 
